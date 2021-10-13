3ce69a213b4e357097c068f082adb56a872ef650
mador.till <mador.till@gmail.com>
2021-10-13 15:07:45 +0300

    תרגול

--- code/practice.js
+++ code/practice.js
@@ -5,11 +5,20 @@ const DATA = {
         "questionsPractice": [
             {
                 type: `multiple`,
-                question: ``,
-                ans1: ``,
-                ans2: ``,
-                ans3: ``,
-                ans4: ``,
+                question: `שאלה 1 מסתבר`,
+                ans1: `תשובה לדוגמא`,
+                ans2: `תשובה לדוגמא`,
+                ans3: `תשובה לדוגמא`,
+                ans4: `תשובה לדוגמא`,
+                correctAns: `ans2`
+            },
+            {
+                type: `multiple`,
+                question: `שאלה 2 כנראה`,
+                ans1: `תשובה נכונה`,
+                ans2: `תשובה לא נכונה`,
+                ans3: `אולי ;)`,
+                ans4: `כדאי ללחוץ עליי`,
                 correctAns: `ans1`
             },
         ],
@@ -232,8 +241,14 @@ const DATA = {
     },
 };
 
+// varubals
+let QUESTIONS = [];
 
+/* onClickPractice
+--------------------------------------------------------------
+Description: */
 const onClickPractice  = () => {
+    strcurrentPage = "practicePage";
     console.log(strCurrentMedType);
     // מעלים מדפים, סמלים בכותרת וכפתורים למטה
     document.querySelector(`.${strCurrentMedType}Shelf`).classList.add("hidden");
@@ -241,14 +256,53 @@ const onClickPractice  = () => {
     document.querySelector(`.shelfsButtons`).classList.add("hidden");
     // משנה מאפיינים של כותרת
     document.querySelector(`.title`).classList.remove("titleMedShelfs");
+    document.querySelector(`.titleConeiner`).classList.add("titleContainerPractice");
     document.querySelector(`.title`).innerHTML = "תרגול";
     // משנה גל וכפתור עליון
     document.querySelector('.wave').setAttribute("src", "../assets/images/grapics/practice/practice-wave.svg");
+    document.querySelector('.topButton').setAttribute("src", "../assets/images/grapics/home-page/right-arrow.svg");
     document.querySelector('.topButton').classList.add("topButtonPractice");
     // מראה דמות ושאלה
     document.querySelector('.practicePage').classList.remove("hidden");
+
+    QUESTIONS = shuffle(DATA[strCurrentMedType]["questionsPractice"]);
+    console.log(QUESTIONS);
+    document.querySelector('.question').innerHTML = QUESTIONS[1]["question"];
     // משנה צבע של הריבועי תשובות
     for (let i = 1; i <= 4; i++) {
         document.querySelector(`.ans${i} img`).classList.add(objMedsShelfsColors[strCurrentMedType][0]);
+        document.querySelector(`.ans${i} div`).innerHTML = QUESTIONS[1][`ans${i}`];
+        document.querySelector(`.ans${i}`).addEventListener("click", onClickAnswer);
     }
+}
+
+/* onClickAnswer
+--------------------------------------------------------------
+Description: */
+const onClickAnswer = (event) => {
+    if (QUESTIONS[1][`correctAns`] == event.currentTarget.classList[1]) {
+        console.log("התשובה נכונה");
+        document.querySelector(`.${event.currentTarget.classList[1]} div`).classList.add("correctAnswer");
+    } else {
+        document.querySelector(`.${event.currentTarget.classList[1]} div`).classList.add("wrongAnswer");
+    }
+}
+
+/*
+shuffle
+------------------------------------------------
+Description: take orgnaiz array and shffel it
+Parameters: array.
+------------------------------------------------
+Programer: Gal
+------------------------------------------------
+*/
+function shuffle(arr) {
+    let tmp = arr.slice();
+    for (let i = 0; i < arr.length; i++) {
+        let index = Math.floor(Math.random() * tmp.length);
+        arr[i] = tmp[index];
+        tmp = tmp.slice(0, index).concat(tmp.slice(index + 1));
+    }
+    return arr;
 }
