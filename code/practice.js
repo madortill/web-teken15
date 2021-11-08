c106a53c604812ba4529fca712024f0673c29d7a
mador.till <mador.till@gmail.com>
2021-11-08 10:04:57 +0200

    שינוי אודות, סידור תוכן בתעודת זהות, שינוי צבעים במבחן

--- code/practice.js
+++ code/practice.js
@@ -2004,7 +2004,7 @@ const startExam = (event) => {
     
     // מראה את סימוני השאלה הנוכחית
     document.querySelector('.testCurrentQuestionDisplay').innerHTML = `שאלה מספר ${currentTestQuestion + 1}`;
-    document.querySelector(`.answerPill${currentTestQuestion + 1}`).style.backgroundColor = "#79BEE0";
+    document.querySelector(`.answerPill${currentTestQuestion + 1}`).style.backgroundColor = "#a3a2a2";
     // מראה את השאלה הנוכחית ומכניס אליה את התוכן
     document.querySelector('.examQuestionContainer .question').innerHTML = arrExamQuestions[currentTestQuestion].question;
     if (arrExamQuestions[currentTestQuestion].type === "binary") {
